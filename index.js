d4c5291d8561fa6fcbd2eb619b8aaa0ce94cb9a7
andyjohn23 <andrewnick906@gmail.com>
2024-03-25 08:26:13 +0300

    all test pass

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const currentUser = "Andrew John";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+const times = 35 * "Hello";
+console.log(times)
+
+const addition = 35 + "Hello";
+console.log(addition)
+
+const number = 27;
+const numberConvert = String(number);
+console.log(typeof numberConvert)
